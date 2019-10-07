ed49f0dd7624dbe7dc267a688faa3272fd9d6000
Renardi <55482726+javorca@users.noreply.github.com>
2019-10-07 11:32:26 +0700

    Update Exercise10.js

--- Exercise10.js
+++ Exercise10.js
@@ -1,19 +1,19 @@
-function perkalianUnik(arr) {
-    var new_arr=[];
-  
-    for(var a=0;a<arr.length;a++){
-      var ar=1;
-      for(var b=0;b<arr.length;b++){
-        if(a !== b){
-          ar *= arr[b];
-        }
+function perkalianUnik(arr){
+  var new_arr = [];
+
+  for (var a = 0; a < arr.length; a++){
+    var ar=1;
+    for(var b = 0; b < arr.length; b++){
+      if(a !== b){
+        ar *= arr[b];
       }
-      new_arr.push(ar);
+    new_arr[a] = ar;
     }
-    return ca;
   }
-  
-  // TEST CASES
+  return new_arr;
+}
+
+// TEST CASES
   console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
   console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
   console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
